d38a05de1eb751a0b6e1da25892f268699fc26f4
fabxx45 <167181241+fabxx45@users.noreply.github.com>
2025-02-27 10:35:24 +0100

    connection string

---
+++ Laufevent/ConnectionString.cs
@@ -0,0 +1,6 @@
+public class ConnectionString
+{
+    public static readonly string connectionstring =
+        "Host=100.122.110.7;Port=5432;Database=laufevent;Username=postgres;Password=Szl-20010901;";
+
+}
