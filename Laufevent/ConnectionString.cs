427fd3b13252796c30ab7e90e236177daae9b097
fabxx45 <167181241+fabxx45@users.noreply.github.com>
2025-06-12 11:58:46 +0200

    change to localhost

--- Laufevent/ConnectionString.cs
+++ Laufevent/ConnectionString.cs
@@ -1,6 +1,6 @@
 public class ConnectionString
 {
     public static readonly string connectionstring =
-        "Host=100.102.196.30;Port=5432;Database=postgres;Username=admin;Password=Szl-20010901;";
+        "Host=127.0.0.1;Port=5432;Database=postgres;Username=admin;Password=Szl-20010901;";
 
 }
