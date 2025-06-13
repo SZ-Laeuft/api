a5ad1a54c83b6358f67e367b3a84b1db8fab2d85
fabxx45 <167181241+fabxx45@users.noreply.github.com>
2025-06-13 08:41:42 +0200

    update update

--- Laufevent/Controllers/UserController.cs
+++ Laufevent/Controllers/UserController.cs
@@ -249,63 +249,66 @@ namespace Laufevent.Controllers
         /// <param name="userInfo">An object containing the updated user details.</param>
         /// <returns>Returns a message indicating the result of the update operation.</returns>
         [HttpPut("{uid}")]
-        [SwaggerOperation(
-            Summary = "Update user information by UID",
-            Description = "Updates user details such as first name, last name, uid, school class, and organization."
-        )]
-        [SwaggerResponse(200, "User details successfully updated.", typeof(string))]
-        [SwaggerResponse(404, "User with the specified UID not found.")]
-        [SwaggerResponse(500, "Internal Server Error - Database issue or unexpected error.")]
-        public async Task<IActionResult> UpdateUserById(decimal uid, [FromBody] UpdateUserModel userInfo)
+[SwaggerOperation(
+    Summary = "Update user information by UID",
+    Description = "Updates user details such as first name, last name, school class, and organization."
+)]
+[SwaggerResponse(200, "User details successfully updated.", typeof(string))]
+[SwaggerResponse(400, "Invalid input data provided.")]
+[SwaggerResponse(404, "User with the specified UID not found.")]
+[SwaggerResponse(500, "Internal Server Error - Database issue or unexpected error.")]
+public async Task<IActionResult> UpdateUserById(decimal uid, [FromBody] UpdateUserModel userInfo)
+{
+    if (userInfo == null)
+    {
+        return BadRequest("The userInfo field is required.");
+    }
+
+    try
+    {
+        using (var connection = new NpgsqlConnection(ConnectionString.connectionstring))
         {
-            try
+            await connection.OpenAsync();
+
+            const string query = @"
+                UPDATE Userinformation
+                SET
+                    firstname = @firstName,
+                    lastname = @lastName,
+                    school_class = @schoolClass,
+                    organisation = @organisation
+                WHERE uid = @uid";
+
+            using (var command = new NpgsqlCommand(query, connection))
             {
-                using (var connection = new NpgsqlConnection(ConnectionString.connectionstring))
+                command.Parameters.AddWithValue("@uid", uid);
+                command.Parameters.AddWithValue("@firstName", userInfo.FirstName ?? (object)DBNull.Value);
+                command.Parameters.AddWithValue("@lastName", userInfo.LastName ?? (object)DBNull.Value);
+                command.Parameters.AddWithValue("@schoolClass", userInfo.SchoolClass ?? (object)DBNull.Value);
+                command.Parameters.AddWithValue("@organisation", userInfo.Organisation ?? (object)DBNull.Value);
+
+                var rowsAffected = await command.ExecuteNonQueryAsync();
+
+                if (rowsAffected > 0)
                 {
-                    await connection.OpenAsync();
-
-                    var query = @"
-                        UPDATE Userinformation 
-                        SET 
-                            firstname = @firstName, 
-                            lastname = @lastName, 
-                            uid = @uid, 
-                            school_class = @schoolClass, 
-                            organisation = @organisation 
-                        WHERE id = @id";
-
-                    using (var command = new NpgsqlCommand(query, connection))
-                    {
-                        command.Parameters.AddWithValue("@uid", uid);
-                        command.Parameters.AddWithValue("@firstName", userInfo.FirstName ?? (object)DBNull.Value);
-                        command.Parameters.AddWithValue("@lastName", userInfo.LastName ?? (object)DBNull.Value);
-                        command.Parameters.AddWithValue("@uid", userInfo.uid ?? (object)DBNull.Value);
-                        command.Parameters.AddWithValue("@schoolClass", userInfo.SchoolClass ?? (object)DBNull.Value);
-                        command.Parameters.AddWithValue("@organisation", userInfo.Organisation ?? (object)DBNull.Value);
-
-                        var rowsAffected = await command.ExecuteNonQueryAsync();
-
-                        if (rowsAffected > 0)
-                        {
-                            return Ok($"User with ID {uid} successfully updated.");
-                        }
-                        else
-                        {
-                            return NotFound($"User with ID {uid} not found.");
-                        }
-                    }
+                    return Ok($"User with UID {uid} successfully updated.");
+                }
+                else
+                {
+                    return NotFound($"User with UID {uid} not found.");
                 }
             }
-            catch (NpgsqlException ex)
-            {
-                return StatusCode(500, $"Database error: {ex.Message}");
-            }
-            catch (Exception ex)
-            {
-                return StatusCode(500, $"An error occurred: {ex.Message}");
-            }
         }
-
+    }
+    catch (NpgsqlException ex)
+    {
+        return StatusCode(500, $"Database error: {ex.Message}");
+    }
+    catch (Exception ex)
+    {
+        return StatusCode(500, $"An error occurred: {ex.Message}");
+    }
+}
         #region Delete User
 
         /// <summary>
